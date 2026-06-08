301ddc7e228797b6ac131bb56b5a9b6ac0931674
clock <clock@wisc.edu>
2026-06-08 11:23:00 -0500

    Fix header image path

--- components/Header/Icon.tsx
+++ components/Header/Icon.tsx
@@ -3,7 +3,7 @@ import React, { CSSProperties } from "react";
 const Icon = ({ size = "140px" }: { size?: CSSProperties["width"] }) => {
   return (
     <img
-      src="/logos/FabAID.svg"
+      src="/images/logos/FabAID.svg"
       alt="FabAID"
       aria-label="FabAID"
       style={{ width: size, height: "auto", display: "block" }}
